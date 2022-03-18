33dbb80d65f11371d125044ec82d3c70edeffb30
Emre Kılıç <emre1kilic@outlook.com>
2022-03-18 14:29:26 +0300

    update q1 and q2
    add sorting to q1

--- H1/q1.m
+++ H1/q1.m
@@ -1,13 +1,15 @@
 clear,clc;
 
-% x = rand(1,100);
-% y= exp(x);
-% plot(x,y);
-% xlabel('x', 'FontSize', 12);
-% ylabel('exp(x)', 'FontSize', 12);
-% print -depsc exp
+x = rand(1,100);
+x = sort(x);
+y= exp(x);
+plot(x,y);
+xlabel('x', 'FontSize', 12);
+ylabel('exp(x)', 'FontSize', 12);
+print -depsc exp
 
 % x = rand(1,100);
+% x = sort(x);
 % y= log(x);
 % plot(x,y);
 % xlabel('x', 'FontSize', 12);
@@ -15,17 +17,19 @@ clear,clc;
 % print -depsc log
 
 % x = rand(1,100);
+% x = sort(x);
 % y = sin(x);
 % plot(x,y);
 % xlabel('x', 'FontSize', 12);
 % ylabel('sin(x)', 'FontSize', 12);
 % print -depsc sin
 
-x = rand(1,100);
-y= cos(x);
-plot(x,y);
-xlabel('x', 'FontSize', 12);
-ylabel('cos(x)', 'FontSize', 12);
-print -depsc cos
+% x = rand(1,100);
+% x = sort(x);
+% y= cos(x);
+% plot(x,y);
+% xlabel('x', 'FontSize', 12);
+% ylabel('cos(x)', 'FontSize', 12);
+% print -depsc cos
 
 
