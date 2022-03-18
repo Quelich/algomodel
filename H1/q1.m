4c87955e3bfb740b6f5e0a1c12356bbc165ec912
Emre Kılıç <emre1kilic@outlook.com>
2022-03-18 11:53:47 +0300

    add eps files for q1

---
+++ H1/q1.m
@@ -0,0 +1,31 @@
+clear,clc;
+
+% x = rand(1,100);
+% y= exp(x);
+% plot(x,y);
+% xlabel('x', 'FontSize', 12);
+% ylabel('exp(x)', 'FontSize', 12);
+% print -depsc exp
+
+% x = rand(1,100);
+% y= log(x);
+% plot(x,y);
+% xlabel('x', 'FontSize', 12);
+% ylabel('log(x)', 'FontSize', 12);
+% print -depsc log
+
+% x = rand(1,100);
+% y = sin(x);
+% plot(x,y);
+% xlabel('x', 'FontSize', 12);
+% ylabel('sin(x)', 'FontSize', 12);
+% print -depsc sin
+
+x = rand(1,100);
+y= cos(x);
+plot(x,y);
+xlabel('x', 'FontSize', 12);
+ylabel('cos(x)', 'FontSize', 12);
+print -depsc cos
+
+
