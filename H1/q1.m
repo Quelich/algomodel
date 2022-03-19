8ac77ebbc1c3928316028326bec662605dff031c
Emre Kılıç <emre1kilic@outlook.com>
2022-03-19 13:39:15 +0300

    update q1
    change font size for labels

--- H1/q1.m
+++ H1/q1.m
@@ -7,29 +7,29 @@ x = sort(x);
 % Exponential Function
 y= exp(x);
 plot(x,y);
-xlabel('x', 'FontSize', 20);
-ylabel('exp(x)', 'FontSize', 20);
+xlabel('x', 'FontSize', 30);
+ylabel('exp(x)', 'FontSize', 30);
 print -depsc exp
 
 % Logarithmic Function
 y= log(x);
 plot(x,y);
-xlabel('x', 'FontSize', 12);
-ylabel('log(x)', 'FontSize', 12);
+xlabel('x', 'FontSize', 30);
+ylabel('log(x)', 'FontSize', 30);
 print -depsc log
 
 % Sine Function
 y= sin(x);
 plot(x,y);
-xlabel('x', 'FontSize', 12);
-ylabel('sin(x)', 'FontSize', 12);
+xlabel('x', 'FontSize', 30);
+ylabel('sin(x)', 'FontSize', 30);
 print -depsc sin
 
 % Logarithmic Function  
 y= cos(x);
 plot(x,y);
-xlabel('x', 'FontSize', 12);
-ylabel('cos(x)', 'FontSize', 12);
+xlabel('x', 'FontSize', 30);
+ylabel('cos(x)', 'FontSize', 30);
 print -depsc cos
 
 
